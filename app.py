ec83dd386115151166f0eb957676bcb262b17fbc
amyqle <amylexoxo@gmail.com>
2026-09-02 17:45:02 -0400

    Modify examples in genre classifier
    Updated example inputs for genre prediction with additional parameters.

--- app.py
+++ app.py
@@ -49,11 +49,11 @@ iface = gr.Interface(
     title="Song Lyrics Genre Classifier",
     description=description,
     examples=[
-        ["I walk this empty street on the boulevard of broken dreams\nWhere the city sleeps and I'm the only one and I walk alone\nMy shadow's the only one that walks beside me\nMy shallow heart's the only thing that's beating"],
-        ["You are my fire, the one desire\nBelieve when I say, I want it that way\nBut we are two worlds apart\nCan't reach to your heart when you say\nThat I want it that way"],
-        ["Swing low, sweet chariot, coming for to carry me home\nSwing low, sweet chariot, coming for to carry me home\nI looked over Jordan, and what did I see\nComing for to carry me home"],
-        ["I got my mind on my money and my money on my mind\nRolling down the street smoking indo, sipping on gin and juice\nLaid back with my mind on my money and my money on my mind"],
-        ["Achy breaky heart, don't tell my heart\nMy achy breaky heart, I just don't think he'd understand\nAnd if you tell my heart, my achy breaky heart\nHe might blow up and kill this man"],
+        ["I walk this empty street on the boulevard of broken dreams\nWhere the city sleeps and I'm the only one and I walk alone\nMy shadow's the only one that walks beside me\nMy shallow heart's the only thing that's beating", 1.0, false],
+        ["You are my fire, the one desire\nBelieve when I say, I want it that way\nBut we are two worlds apart\nCan't reach to your heart when you say\nThat I want it that way", 1.0, false],
+        ["Swing low, sweet chariot, coming for to carry me home\nSwing low, sweet chariot, coming for to carry me home\nI looked over Jordan, and what did I see\nComing for to carry me home", 1.0, false],
+        ["I got my mind on my money and my money on my mind\nRolling down the street smoking indo, sipping on gin and juice\nLaid back with my mind on my money and my money on my mind", 1.0, false],
+        ["Achy breaky heart, don't tell my heart\nMy achy breaky heart, I just don't think he'd understand\nAnd if you tell my heart, my achy breaky heart\nHe might blow up and kill this man", 1.0, false],
     ],
     flagging_mode="never",
 )
